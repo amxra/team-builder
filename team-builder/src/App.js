f1202170c498b304d363a2b8d36b48044d27cfd9
amxra <amiradediran@gmail.com>
2019-09-19 11:03:15 +0100

    Add useState hook in app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import uuid from "uuid";
+
+cont initialMembers = [
+  {id: uuid(), name: "Amira", Age: "2", Role: "Frontend"},
+  {id: uuid(), name: "Amira", Age: "2", Role: "Frontend"},
+  {id: uuid(), name: "Amira", Age: "2", Role: "Frontend"}
+]
+
+const [members, setMembers] = useState(initialMembers);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
