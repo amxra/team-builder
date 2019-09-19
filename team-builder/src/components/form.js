6833e2eb2851b0a49d72a6c1a08d9e28ff49ac54
amxra <amiradediran@gmail.com>
2019-09-19 11:20:26 +0100

    Create initial state for memberForm and useState in form component

---
+++ team-builder/src/components/form.js
@@ -0,0 +1,14 @@
+
+import React, {useState} from "react";
+
+const initialMemberForm = {
+    name: ' ',
+    age: ' ',
+    role: ' '
+};
+
+function Form (props){
+    const [memberForm, setMemberForm] = useState(initialMemberForm);
+    
+    return()
+}
