c94ebc132ce696b605c0fb5eee8afda6464b4f68
amxra <amiradediran@gmail.com>
2019-09-19 11:24:20 +0100

    Build form structure and export form

--- team-builder/src/components/form.js
+++ team-builder/src/components/form.js
@@ -10,5 +10,16 @@ const initialMemberForm = {
 function Form (props){
     const [memberForm, setMemberForm] = useState(initialMemberForm);
     
-    return()
+    return(
+        <form>
+            <label htmlFor = "nameInput">Name</label>
+            <input value = {memberForm.name}/>
+            <label htmlFor = "ageInput">Name</label>
+            <input value = {memberForm.age}/>
+            <label htmlFor = "roleInput">Name</label>
+            <input value = {memberForm.role}/>
+        </form>
+    )
 }
+
+export default Form;
